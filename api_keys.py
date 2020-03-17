73d81be8507c380a45120eb2842a5837f01e70a9
gustavhempel <t.mahn@oyacapitalinc.com>
2020-03-17 11:16:26 -0400

    added the coordinates of all the wineries and a map where they are

---
+++ api_keys.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+#weather_api_key = "ab6907d289e10d714a6e88b30761fae22"
+weather_api_key = "a4830e9aa4deb6b1bcb21c9475157b31"
+
+# Google API Key
+g_key = "AIzaSyBxMFase9yLbhIfvYy3C96J9ukUWadOcOs"
